89a83d8aeebeeb501eb226728b4a3ba7653c8410
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-10-28 22:28:55 -0400

    get our key setup

--- services/cache.js
+++ services/cache.js
@@ -5,7 +5,11 @@ const exec = mongoose.Query.prototype.exec;
 mongoose.Query.prototype.exec = function() {
     console.log('IM about to run a query');
 
-    console.log(this.getQuery());
-    console.log(this.mongooseCollection.name);
+    const key = Object.assign({}, this.getQuery(), {
+        collection: this.mongooseCollection.name
+    });
+
+    console.log(key)
+
     return exec.apply(this, arguments);
 }
