0f88568df71e1c7d6ea3cf38885e7be42a5a8b86
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-10-28 22:19:37 -0400

    show collection and query pieces

--- services/cache.js
+++ services/cache.js
@@ -5,5 +5,7 @@ const exec = mongoose.Query.prototype.exec;
 mongoose.Query.prototype.exec = function() {
     console.log('IM about to run a query');
 
+    console.log(this.getQuery());
+    console.log(this.mongooseCollection.name);
     return exec.apply(this, arguments);
 }
