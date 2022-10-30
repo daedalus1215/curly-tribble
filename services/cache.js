b0080dd9325c39989273907fe84966896dcdee9f
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-10-30 10:46:56 -0400

    hydrate single and array

--- services/cache.js
+++ services/cache.js
@@ -1,15 +1,30 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
 
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function() {
-    console.log('IM about to run a query');
-
+mongoose.Query.prototype.exec = async function() {
     const key = Object.assign({}, this.getQuery(), {
         collection: this.mongooseCollection.name
     });
 
-    console.log(key)
+    // See if we have a value for 'key' in redis
+    const cacheValue = await client.get(key);
 
-    return exec.apply(this, arguments);
+    if (cacheValue) {
+        const doc = JSON.parse(cacheValue);
+        return Array.isArray(doc) 
+            ? doc.map(d => new this.model(d))
+            : new this.model(doc);
+    }
+
+    // If we do, return that; Otherwise, issue the query and store the result in redis.
+    const result = await exec.apply(this, arguments);
+    client.set(key, JSON.stringify(result));
+
+    return result;
 }
