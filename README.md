f0d65cce7aa91cabf41eff1287b150a1611201ef
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-10-30 10:57:05 -0400

    make it so cache is optional

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AdvancedNodeStarter
 Starting project for a course on Advanced Node @ Udemy
+
+# External Required Services
+* MongoDB
+* Redis
--- services/cache.js
+++ services/cache.js
@@ -7,12 +7,22 @@ const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
+
+mongoose.Query.prototype.cache = function () {
+    this.useCache = true; // keep track if consumer wants to leverage caching or not.
+    return this; // make it chainable.
+};
+
+
 mongoose.Query.prototype.exec = async function() {
+    if (!this.useCache) {
+        return exec.apply(this, arguments);
+    }
+
     const key = Object.assign({}, this.getQuery(), {
         collection: this.mongooseCollection.name
     });
 
-    // See if we have a value for 'key' in redis
     const cacheValue = await client.get(key);
 
     if (cacheValue) {
@@ -22,9 +32,7 @@ mongoose.Query.prototype.exec = async function() {
             : new this.model(doc);
     }
 
-    // If we do, return that; Otherwise, issue the query and store the result in redis.
     const result = await exec.apply(this, arguments);
     client.set(key, JSON.stringify(result));
-
     return result;
-}
+};
