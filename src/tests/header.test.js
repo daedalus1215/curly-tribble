aa8838ecc1a9c00e13e8251b020d0088080c5822
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-11-22 21:26:04 -0500

    Oauth start test

--- src/tests/header.test.js
+++ src/tests/header.test.js
@@ -6,22 +6,33 @@ let page;
 beforeEach(async () => {
     // Arrange
     browser = await puppeteer.launch({
-        headless:false
+        headless: false
     });
     page = await browser.newPage();
-    
+
     await page.goto('http://localhost:3000'); // going to the page is going to some amount of time, so we want to await.
 
 });
 
-afterEach(async() => {
+afterEach(async () => {
     await browser.close();
 });
 
 test('should find header on homepage', async () => {
-    // Arrange
-    
+    // Act
     const text = await page.$eval('a.brand-logo', el => el.innerHTML);
 
+    // Assert
     expect(text).toEqual('Blogster');
-})  
+});
+
+test('clicking login starts oauth flow', async () => {
+    // Arrange
+
+    // Act
+    await page.click('.right a ');
+    const url = await page.url();
+
+    // Assert
+    expect(url).toContain('https://accounts.google.com/o/oauth2/v2/auth');
+});
