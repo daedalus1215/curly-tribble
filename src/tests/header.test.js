43bbe0c1b2061688482d212e8b8e28401beaf612
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-11-22 21:12:41 -0500

    Assert on element on page

--- src/tests/header.test.js
+++ src/tests/header.test.js
@@ -1,10 +1,16 @@
 const puppeteer = require('puppeteer');
 
-test('what', async () => {
+test('should find header on homepage', async () => {
+    // Arrange
     const browser = await puppeteer.launch({
         headless:false
     });
     const page = await browser.newPage();
-    await page.goto('http://localhost:3000'); // going to the page is going to some amount of time, so we want to await.
     
+    // Act
+    await page.goto('http://localhost:3000'); // going to the page is going to some amount of time, so we want to await.
+
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+
+    expect(text).toEqual('Blogster');
 })  
