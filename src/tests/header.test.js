f85518d03fc9274ddcac292c601e0466c975dfea
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-11-22 21:15:49 -0500

    Add beforeEach for the browser and visiting the homepage

--- src/tests/header.test.js
+++ src/tests/header.test.js
@@ -1,15 +1,22 @@
 const puppeteer = require('puppeteer');
 
-test('should find header on homepage', async () => {
+let browser;
+let page;
+
+beforeEach(async () => {
     // Arrange
-    const browser = await puppeteer.launch({
+    browser = await puppeteer.launch({
         headless:false
     });
-    const page = await browser.newPage();
+    page = await browser.newPage();
     
-    // Act
     await page.goto('http://localhost:3000'); // going to the page is going to some amount of time, so we want to await.
 
+});
+
+test('should find header on homepage', async () => {
+    // Arrange
+    
     const text = await page.$eval('a.brand-logo', el => el.innerHTML);
 
     expect(text).toEqual('Blogster');
