6ef253b776c275fdd857a7f2cbbf2bfbea50ea96
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-11-29 22:10:29 -0500

    Update blog.test.js, so we follow unhappy form path.

--- src/tests/blogs.test.js
+++ src/tests/blogs.test.js
@@ -22,4 +22,18 @@ describe('when logged in', async () => {
         expect(actual).toEqual('Blog Title');
         browser.close();
     });
+
+    describe('And using invalid inputs', async () => {
+        beforeEach(async () => {
+            await page.click('form button');
+        });
+
+        test('the form shows an error message', async () => {
+            const titleError = await getContents(page, '.title .red-text');
+            const contentError = await getContents(page, '.content .red-text');
+
+            expect(titleError).toEqual('You must provide a value');
+            expect(contentError).toEqual('You must provide a value');
+        })
+    })
 });
