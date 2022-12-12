ac9ca454ffa9ed4bbe34b58a1dbfc78936b6fb44
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-12-11 20:28:13 -0500

    Up BlogFormReview: Add image upload button spike

--- client/src/components/blogs/BlogFormReview.js
+++ client/src/components/blogs/BlogFormReview.js
@@ -7,6 +7,9 @@ import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
 
 class BlogFormReview extends Component {
+  state = { file: null };
+
+
   renderFields() {
     const { formValues } = this.props;
 
@@ -47,12 +50,27 @@ class BlogFormReview extends Component {
     submitBlog(formValues, history);
   }
 
+  onFileChange(event) {
+    this.setState({ file: event.target.files[0] });
+    const { submitBlog, history, formValues } = this.props;
+    submitBlog(formValues, this.state.file, history);
+  }
+
   render() {
     return (
       <form onSubmit={this.onSubmit.bind(this)}>
         <h5>Please confirm your entries</h5>
         {this.renderFields()}
 
+        <div>
+          <h5>Add an Image</h5>
+          <input
+            name="file"
+            type="file"
+            accept="image/*"
+            onChange={this.onFileChange.bind(this)}
+          />
+        </div>
         {this.renderButtons()}
       </form>
     );
