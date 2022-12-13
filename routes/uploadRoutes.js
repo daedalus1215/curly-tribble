f6a9c548c2b6af144a2227de7d0f657c45833f48
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-12-12 21:56:08 -0500

    Register upload Routes

---
+++ routes/uploadRoutes.js
@@ -0,0 +1,21 @@
+const AWS = require('aws-sdk');
+const keys = require('../config/keys');
+const uuid = require('uuid');
+const requireLogin = require('../middlewares/requireLogin');
+const s3 = new AWS.S3({
+    accessKeyId: keys.accessKeyId,
+    secretAccessKey: keys.secretAccessKey,
+});
+
+module.exports = app => {
+    app.get('/api/upload', requireLogin, (req, res) => {
+        const key = `${req.user.id}/${uuid.v1()}.jpeg`;
+
+        // Get a presigned url and respond back to the client with it.
+        s3.getSignedUrl('putObject', {
+            Bucket: 'my-blog-bucket-123',
+            ContentType: 'jpeg',
+            Key: key
+        }, (err, url) => res.send({ key, url }));
+    });
+};
